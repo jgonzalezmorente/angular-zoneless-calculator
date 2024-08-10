abc499b279f1548b8caabea71e67a3be9cbe826b
jgonzalezmorente <jgonzalezmorente@gmail.com>
2024-08-10 09:01:07 +0200

    Funcionalidad

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, input, output, signal, viewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -8,21 +8,44 @@ import { ChangeDetectionStrategy, Component, HostBinding, input, ViewEncapsulati
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './calculator-button.component.css',
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400'    
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-1/4]': '!isDoubleSize()',
+    '[class.w-2/4]': 'isDoubleSize()',
   },
   // encapsulation: ViewEncapsulation.None
 })
 export class CalculatorButtonComponent {
+  public isPressed = signal(false);
+  public onClick = output<string>();
+  public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
   public isCommand = input(false, {
     transform: (value: boolean | string) => typeof value === 'string' ? value === '' : value
   });
-
   public isDoubleSize = input(false, {
     transform: (value: boolean | string) => typeof value === 'string' ? value === '' : value
   });
   
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize();
+  // @HostBinding('class.w-2/4') get commandStyle() {
+  //   return this.isDoubleSize();
+  // }
+  
+  public handleClick(event?: Event) {
+    if (event) {      
+      (event.target as HTMLElement).blur();
+    }
+    if (!this.contentValue()?.nativeElement) return;
+    const value = this.contentValue()!.nativeElement.innerText;    
+    this.onClick.emit(value.trim());
   }
 
+  public keyboardPressedStyle(key: string) {
+    if (!this.contentValue()) return;
+    const value = this.contentValue()?.nativeElement.innerText;
+    if (value !== key) return;
+    this.isPressed.set(true);
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 100);
+  }
+  
 }
