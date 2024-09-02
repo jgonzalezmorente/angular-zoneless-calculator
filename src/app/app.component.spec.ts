c55db8422427c9dfc4f7131a50a0b5bd45b5d850
jgonzalezmorente <jgonzalezmorente@gmail.com>
2024-09-02 09:37:24 +0200

    Pruebas AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,41 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    compiled = fixture.nativeElement as HTMLElement;    
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should create the app', () => {    
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'zoneless-calculator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should be 3', () => {
+    // A = Arrange
+    const num1 = 1;
+    const num2 = 2;
+
+    // A = Act
+    const result = num1 + num2;
+
+    // A = Assert
+    expect(result).toBe(3);
+  });
+
+  it(`should have the 'zoneless-calculator' title`, () => {    
     const app = fixture.componentInstance;
     expect(app.title).toEqual('zoneless-calculator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
+  it('should render router-outlet', () => {
+    expect(compiled.querySelector('router-outlet')).not.toBeNull();
   });
 });
